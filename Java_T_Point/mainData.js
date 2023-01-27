d37427aaa37022ec883c76674a34498e4d550a6e
Shivam Kashyap <shivamkumarkashyap12@gmail.com>
2023-01-28 02:04:15 +0530

    Small update with sideNavigation

--- Java_T_Point/mainData.js
+++ Java_T_Point/mainData.js
@@ -1,8 +1,14 @@
 let contentOnly = document.querySelector(".onlycontent").style
+let sideMenu = document.querySelector("#menu");
 console.log("Java_T_point is fetched");
 
 function Java_T_Point(){
     contentOnly.minWidth = "75%"
+    sideMenu.style.position = "sticky"
+    sideMenu.style.top = "3rem"
+    sideMenu.style.overflowY = "scroll";
+    sideMenu.style.overflowX = "hidden";
+    sideMenu.style.height = "calc(100vh - 8rem)";
     console.log("Java_T_point is executed");
 }
 
